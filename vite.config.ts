a374bb1dd856e6d713b0c4bb839f0bbf5560ab68
timotismjntk <timotiussimanjuntak24@gmail.com>
2025-11-11 16:32:29 +0700

    tambah workflow

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: '/undangan-pentahbisan-gereja-gbi-angkatan-66/',
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
